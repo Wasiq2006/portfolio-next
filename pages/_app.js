5659359d7d5998510b73d6815e14c65c17a99b79
Muhamamd Wasiq <wasiqmansoor69@gmail.com>
2026-01-05 21:31:10 +0500

    Fix layout: remove global MatrixBackground from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
 import '../styles/globals.css';
-import MatrixBackground from '../components/MatrixBackground';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div className="relative">
-      {/* Background always behind */}
-      <MatrixBackground />
-
-      {/* Page content always above */}
-      <div className="relative z-10">
-        <Component {...pageProps} />
-      </div>
-    </div>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
